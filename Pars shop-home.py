20bb0ea772afab584763e140a6d9bcfa7d1df58f
MicraKS <5328207@gmail.com>
2023-03-13 13:34:35 +0300

    main file

--- Pars shop-home.py
+++ Pars shop-home.py
@@ -1,9 +1,9 @@
-import requests
 from bs4 import BeautifulSoup
-import json
 import csv
 
+
 def get_data(url):
+    # Парсим данные
     # headers = {
     #     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
     # }
@@ -12,52 +12,50 @@ def get_data(url):
     # with open("test.html", "w", encoding='utf-8') as file:
     #     file.write(requestss.text)
 
+    #  Сохраняем в файл
     with open("test.html", encoding='utf-8') as file:
         scr = file.read()
 
-    home = []
-    title = []
-    link = []
-    vendor_code = []
-    square = []
-    price = []
+    # Создаем табличку с заголовками
+    with open(f"venv/Дома.csv", "w", newline='', encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow(
+            (
+                "Title",
+                "Link",
+                "Square",
+                "Price",
+                "Vendor_code"
+            )
+        )
 
     soup = BeautifulSoup(scr, "lxml")
     src = soup.find_all("a", class_="element")
 
+    # Проходим куску кода для поиска нужной информации
     for item in src:
-        link.append("https://www.shop-project.ru" + item.get("href"))
-        title.append(item.get("title"))
+        link = "https://www.shop-project.ru" + item.get("href")
 
-    vendor_code_pars = soup.find_all("span", class_="default-color")
+        title = item.get("title")
 
-    for item in vendor_code_pars:
-        vendor_code.append(item.text)
+        square = item.find("td", class_="element-square").text
 
-    square_pars = soup.find_all("span", class_="color-white")
-    # sup = soup.find("span", class_="color-white").find("sup").text
-    # print(sup)
+        price = item.find("span", class_="default-color-2").text
 
-    for item in square_pars:
-        square.append(item.text)
+        vendor_code = item.find("span", class_="default-color").text
 
-    # print(square)
-    price_pars = soup.find_all("span", class_="default-color-2")
-    for item in price_pars:
-        price.append(item.text)
-
-    for i in range(len(title)):
-        home.append(
-                    {
-                        "Title": title[i],
-                        "link": link[i],
-                        "data": vendor_code[i],
-                        "square": square[i],
-                        "price": price[i],
-                    }
+        # Записываем информацию в табличку с заголовками
+        with open(f"venv/Дома.csv", "a", newline='', encoding="utf-8") as file:
+            writer = csv.writer(file)
+            writer.writerow(
+                (
+                    title[(str(title).find(".")) + 2:].strip(),
+                    link.strip(),
+                    square.strip(),
+                    price.strip(),
+                    vendor_code.strip(),
                 )
-    print(home)
-
+            )
 
 
 get_data("https://www.shop-project.ru/")
